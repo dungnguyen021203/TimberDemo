69a82e8bcc721cf12b47c05904ddb5a01769fc77
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-09-03 14:08:28 +0700

    Timber + Logger

--- app/src/main/java/com/example/timberdemo/MainActivity.kt
+++ app/src/main/java/com/example/timberdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.timberdemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,16 +12,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.example.timberdemo.ui.theme.TimberDemoTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TimberDemoTheme {
-                Timber.d("MainActivity created!")
+                Timber.d("onCreate: inside MainActivity")
+
+                lifecycleScope.launch(Dispatchers.IO) {
+                    Timber.d("onCreate: inside launch IO")
+                }
+
+                thread {
+                    Timber.d("onCreate: inside thread")
+                }
             }
         }
     }
--- app/src/main/java/com/example/timberdemo/TimberApp.kt
+++ app/src/main/java/com/example/timberdemo/TimberApp.kt
@@ -1,20 +1,30 @@
 package com.example.timberdemo
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import timber.log.Timber
 
 class TimberApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)
+            .methodCount(1)
+            .methodOffset(5)
+            .tag("")
+            .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+
         Timber.plant(object: Timber.DebugTree(){
-
-
             /**
              * Override [log] to modify the tag and add a "global tag" prefix to it. You can rename the String "global_tag_" as you see fit.
              */
             override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                super.log(priority, "global_tag_$tag", message, t)
+                Logger.log(priority, "-$tag", message, t)
             }
 
             /**
